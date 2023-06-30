c1aab77954245a90a5b92134596ba06347165691
anniinakinnunen <anniina.he.kinnunen@gmail.com>
2023-06-30 18:08:03 +0300

    Test

--- index.js
+++ index.js
@@ -40,6 +40,10 @@ app.get('/api/persons', (req, res) => {
   res.json(persons)
 })
 
+app.get('/', (req, res) => {
+  res.json(persons)
+})
+
 app.get('/api/persons/:id', (req, res) => {
   const id = Number(req.params.id)
   const person = persons.find(person => person.id === id)
