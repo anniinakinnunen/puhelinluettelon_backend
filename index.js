428de46f966de03936d3f0b4c39ffea473ac09d7
anniinakinnunen <anniina.he.kinnunen@gmail.com>
2023-06-30 11:20:49 +0300

    Exercise 3.7 correction

--- index.js
+++ index.js
@@ -2,17 +2,8 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 
-morgan.token('myToken', function (req, res) {
-  return (JSON.stringify(req.method))
-  // if (req.method === 'POST') {
-  //   return 'posting'
-  // } else {
-  //   return 'not posting'
-  // }
-})
-
 app.use(express.json())
-app.use(morgan('myToken'))
+app.use(morgan('tiny'))
 
 let persons = [
   {
